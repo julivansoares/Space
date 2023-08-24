6bf5ff6ba6b873506a739da01c93a80813c1a8a2
julivansoares <julivan.soares@outlook.com>
2023-08-24 19:33:43 -0300

    atualização do projeto

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import PaginaInicial from "./paginas/pagina_inicial";
+import './styles/estilosGlobais.scss'
+
+function App() {
+  return (
+    <PaginaInicial/>
+  );
+}
+
+export default App;
---
+++ src/paginas/pagina_inicial/index.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+
+
+
+
+export default function PaginaInicial(){
+
+    return(
+         <>
+            <Cabecalho/>
+            <Menu/>
+        </>
+    )
+}
